819dc806e7ae3d0b51aa8adae94db82b4d5cc78e
tarrras8 <140727620+tarrras8@users.noreply.github.com>
2023-09-19 15:33:14 +0300

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        PrintReverse(N);
+
+        Console.ReadKey();
+    }
+
+    static void PrintReverse(int num)
+    {
+        if (num == 1)
+        {
+            Console.Write(num + " ");
+        }
+        else
+        {
+            PrintReverse(num - 1);
+            Console.Write(num + " ");
+        }
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,29 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+using System;
+
+namespace SumOfNaturals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение M:");
+            int M = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение N:");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = M; i <= N; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine($"Сумма натуральных чисел в промежутке от {M} до {N} равна: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
+
+
